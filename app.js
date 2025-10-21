8a86ba741f365c940a9c3ef6e3fd7d709be41fc1
hoatd <tranduyhoa94@gmail.com>
2025-10-21 21:30:47 +0700

    Test Action

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker by Hoa update 1! 🐳');
+  res.send('Hello from Docker by Hoa update 2! 🐳');
 });
 
 app.get('/health', (req, res) => {
